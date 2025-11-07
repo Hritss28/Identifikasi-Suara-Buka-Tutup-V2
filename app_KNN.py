d4dc489dd2f5df66382d78bfee01e99f439f7fff
Haritss28 <230411100003@student.trunojoyo.ac.id>
2025-11-07 20:29:16 +0700

    Update Code

--- app_KNN.py
+++ app_KNN.py
@@ -148,7 +148,7 @@ def verify_speaker(y, sr, speaker_profiles, is_recording=False):
         return False, "unknown", 0.0
     
     if is_recording:
-        distance_threshold = 0.55
+        distance_threshold = 0.45
         gap_threshold = 0.02
         similarity_threshold = 0.88
         st.sidebar.info("**RECORDING MODE**")
